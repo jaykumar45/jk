3c38aaee5f6db8a1f4ead5c675be852df3a837dd
jayku <jaykumargupta4512@gmail.com>
2023-07-25 11:34:34 +0530

    change the commit

--- my portfolio/main.js
+++ my portfolio/main.js
@@ -1,22 +1,12 @@
-// alert("")
-let countEl = document.getElementById("count-el")
-let count = 0;
-function increment(){
-    count = count + 1;
-    countEl.innerText = count
-}
+const menuLog = document.querySelector(".hamburger");
+const menuItems =document.querySelector(".nav-menu");
 
+menuLog.addEventListener("click",() => {
+    menuLog.classList.toggle("active");
+    menuItems.classList.toggle("active");
+})
 
-
-
-const  apearNav =  document.getElementById("menu-toggle");
-const nav_header = document.getElementById("main-nav")
-
-
-const  toggleNavbar = () => {
-    // alert('hey bro')
-    nav_header.toggle("active")
-}
-     
-
-     apearNav = addEventListener('click',() => toggleNavbar())
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",() => {
+    menuLog.classList.remove("active");
+    menuItems.classList.remove("active");   
+}))
